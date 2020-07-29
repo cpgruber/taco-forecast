3685de87251643eaf3c4883e1fc9725dc118d1c5
Chase Gruber <cgruber@esri.com>
2020-07-29 15:05:47 -0400

    Update update-status.js

--- scripts/update-status.js
+++ scripts/update-status.js
@@ -1,4 +1,10 @@
 #!/usr/bin/env node
 
-console.log(JSON.stringify(process));
-console.log(process.env.BODY, process.env.ISSUE);
+const ISSUE = 4;
+const TRIGGER = parseInt(process.env.ISSUE, 10)
+const BODY = process.env.BODY || '';
+const parsed = BODY.match(/^(OPEN|BLOCKED)/);
+const which = parsed && parsed[0];
+if (TRIGGER === ISSUE && which) {
+  console.log(which);
+}
